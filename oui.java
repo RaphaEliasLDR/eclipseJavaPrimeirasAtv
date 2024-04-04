5f3ada321a254459993c37fa70cb7ece2092c9a2
Raphael Elias <143661944+RaphaEliasLDR@users.noreply.github.com>
2024-04-04 11:01:24 -0300

    Update oui.java

--- oui.java
+++ oui.java
@@ -2,28 +2,29 @@ import java.util.Scanner;
 public class oui { //oui foi um nome aleatorio para a pasta.
 public static void main (String[] args) {
 
-	Scanner scanner = new Scanner (System.in);
+Scanner scanner = new Scanner (System.in);
 	
-	int numNotas = 5;
-	double totalNotas = 0;
-	int contador = 0;
+int numNotas = 5;
+double totalNotas = 0;
+int contador = 0;
 	
-			while (contador < numNotas){
-				contador++;
-				System.out.println("Digite a nota" +contador+ ":");
-				double nota = scanner.nextDouble();
-				totalNotas += nota;
-			}
-	double media = totalNotas / numNotas;
-	System.out.println ("A média das notas é: " +media);
-	scanner.close();
+while (contador < numNotas){
+contador++;
+System.out.println("Digite a nota" +contador+ ":");
+double nota = scanner.nextDouble();
+totalNotas += nota;
+}
+double media = totalNotas / numNotas;
+System.out.println ("A média das notas é: " +media);
+scanner.close();
 
-	if (media >= 7) {
-		System.out.println ("Aprovado!");
-	} else if (media < 7 && media >= 4) {
-		System.out.println ("Reprovado com dieito a recupeção");
-	} else if (media < 4) {
-		System.out.println ("Reprovado sem direito a recuperação.");
-	}
+if (media >= 7) {
+System.out.println ("Aprovado!");
+} else if (media < 7 && media >= 4) {
+System.out.println ("Reprovado com dieito a recupeção");
+} else if (media < 4) {
+System.out.println ("Reprovado sem direito a recuperação.");
+}
+
 }
 }
